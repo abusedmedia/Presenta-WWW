6ff57769ebc0cadedc96a934d25fc72f43a2d266
abusedmedia <ff@todo.to.it>
2019-09-14 19:45:25 +0200

    refactoring

---
+++ layouts/legal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <HeaderGlobal />
+        <div class="body">
+            <nuxt class="page" />
+            <PageLegalFooter class="page" />
+            <FooterComp />
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+import HeaderGlobal from '~/components/HeaderGlobal.vue'
+import FooterComp from '~/components/FooterComp.vue'
+import PageLegalFooter from '~/components/PageLegalFooter.vue'
+
+export default {
+    components:{
+        HeaderGlobal,FooterComp,PageLegalFooter
+    },
+}
+</script>
--- pages/terms-of-service.vue
+++ pages/terms-of-service.vue
@@ -1,29 +1,13 @@
 <template>
-    <div id="app" :class="colorstyle">
-      <HeaderGlobal />
-
-      <div class="body">
-        <div v-html="md" class="page"></div>
-        <PageLegalFooter class="page" />
-        <FooterComp />
-      </div>
-  </div>
+    <div v-html="md"></div>
 </template>
 
 <script>
 import MD from './terms-of-service.md'
 
-import HeaderGlobal from '~/components/HeaderGlobal.vue'
-import FooterComp from '~/components/FooterComp.vue'
-import PageLegalFooter from '~/components/PageLegalFooter.vue'
-import { mapGetters } from 'vuex'
-
 export default {
-  components:{
-    HeaderGlobal,FooterComp,PageLegalFooter
-  },
+  layout: 'legal',
   computed:{
-      ...mapGetters(['colorstyle']),
       md(){
         return MD
       }
