8a916900d803ec509feac21c39e13c750ebbe45d
abusedmedia <ff@todo.to.it>
2020-10-30 08:07:39 +0100

    copy

--- components/Opening.vue
+++ components/Opening.vue
@@ -2,10 +2,10 @@
         <div class="wrap">
             <div class="page">
                 <div class="text">
-                    <p class="main">We believe that presentations can be experienced differently.</p>
+                    <p class="main">We believe that presentations can be built and experienced differently.</p>
                     <p>More then 30 years ago <b>PowerPoint set the bar</b> about how presentation documents should be crafted.</p>
-                    <p>After those years, we still <b>move objects on canvas</b> to create documents to support our communication needs.</p>
-                    <p>Nowadays, we are in a position to <b>envision different ways</b> to build decks, ways that are more effective, efficient and fun as well.</p>
+                    <p>After that, we still <b>move objects on canvas</b> to create documents for our communication needs.</p>
+                    <p>Nowadays, we can <b>envision different ways</b> to build decks, ways that are more effective, efficient and fun as well.</p>
                     <p>It’s a long ride and our open-source javascript library, <a href="https://lib.presenta.cc/">PRESENTA Lib</a>, marks another milestone in this journey.</p>
                 </div>
             </div>
