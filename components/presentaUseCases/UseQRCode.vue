bb04512445257c465b808d6a07fb9804d90e8c43
abusedmedia <hello@fabiofranchino.com>
2022-01-16 13:57:32 +0100

    minor

--- components/presentaUseCases/UseQRCode.vue
+++ components/presentaUseCases/UseQRCode.vue
@@ -40,9 +40,9 @@ export default {
         return {
             images:[
                 {
-                    a:'https://cloud.presenta.cc/v1/url/zGywhb2oJn:z2O9wALBl?&code=aa&bg=rect:fill:AliceBlue&subtitle=1st Novembre 2021&img=https://source.unsplash.com/random/700x350',
-                    b:'https://cloud.presenta.cc/v1/url/zGywhb2oJn:z2O9wALBl?&code=eqwjegqwyegqw&bg=rect:fill:Beige&subtitle=2nd Novembre 2021&img=https://source.unsplash.com/random/701x350',
-                    c:'https://cloud.presenta.cc/v1/url/zGywhb2oJn:z2O9wALBl?&code=2883774&bg=rect:fill:Cornsilk&subtitle=3rd Novembre 2021&img=https://source.unsplash.com/random/702x350'
+                    a:'https://cloud.presenta.cc/v1/url/zGywhb2oJn:z2O9wALBl?&code=aaw&bg=rect:fill:AliceBlue&subtitle=1st Novembre 2021&img=https://source.unsplash.com/random/700x350',
+                    b:'https://cloud.presenta.cc/v1/url/zGywhb2oJn:z2O9wALBl?&code=eqwj2egqwyegqw&bg=rect:fill:Beige&subtitle=2nd Novembre 2021&img=https://source.unsplash.com/random/701x350',
+                    c:'https://cloud.presenta.cc/v1/url/zGywhb2oJn:z2O9wALBl?&code=28837741&bg=rect:fill:Cornsilk&subtitle=3rd Novembre 2021&img=https://source.unsplash.com/random/702x350'
                 }
             ]
         }
