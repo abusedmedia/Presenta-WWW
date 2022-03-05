f9e6d86c547b90e68d24d8c338997d26fe4733ab
abusedmedia <hello@fabiofranchino.com>
2022-03-05 18:12:39 +0100

    upd

---
+++ pages/features/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="list">
+
+        <div class="hero">
+            <h1>API Features</h1>
+            <h2>Discover the PRESENTA Generation API</h2>
+        </div>
+        
+        <ul class="grid">
+            <li v-for="post in posts" :key="post.url">
+                <nuxt-link :to="post.url">
+                    <div class="img">
+                        <img src="/500x500.png" />
+                    </div>
+                    <p>{{post.title}}</p>                
+                </nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+  layout: 'commonPage',
+  head () {
+        return {
+            title: 'All Features of PRESENTA Generation API'
+        }
+  },
+  async asyncData(){
+    let res = await import(`~/content/blog/list.json`)
+    return {
+        posts: [
+            {title:'Document Generation API', url:'/features/document-generation-api'},
+            {title:'Image Generation API', url:'/features/image-generation-api'},
+            {title:'Open-Graph Generation API', url:'/features/open-graph-generation-api'},
+            {title:'Certificate Generation API', url:'/features/certificate-generation-api'},
+            {title:'PDF Generation API', url:'/features/pdf-generation-api'},
+            {title:'QR-Codes Generation API', url:'/features/qr-codes-generation-api'},
+            {title:'Social Preview Generation API', url:'/features/social-preview-generation-api'}
+        ]
+    }
+  }
+}
+</script>
+
+
+
+
+
+<style scoped>
+
+.list{
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center
+}
+</style>
